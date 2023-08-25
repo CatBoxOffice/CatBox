50f9b84a50541db70dc5dfa21b6413e4a72a05df
JamesGauvreau <james.longfrenchname@gmail.com>
2023-08-25 13:47:33 -0700

    fixed navbar paths

--- client/src/components/bars/Navbar.jsx
+++ client/src/components/bars/Navbar.jsx
@@ -13,30 +13,30 @@ function NavBar() {
       <div id="navbar">
         {/* ↓ We should add a logo here, if we're going to use one. ↓ */}
         <Link to="/">Home</Link>
-        <Link to="/">All Movies</Link>
+        <Link to="/AllMovies">All Movies</Link>
         {/* <Link to="/">TopGenres</Link>   <-- component doesn't exist yet*/}
-        <Link to="/">Rating Rubric</Link>
+        <Link to="/RatingRubric">Rating Rubric</Link>
         {/* ↓ The following needs to appear to admins, and only admins ↓ */}
-        <Link to="/">All Users</Link>
+        <Link to="/AllUsers">All Users</Link>
         {/* ↓ The following need to alternate according to whether the user is 
         logged in. ↓ */}
-        <Link to="/">Log In</Link>
-        <Link to="/">Profile Page</Link>
+        <Link to="/LogIn">Log In</Link>
+        <Link to="/Profile">Profile Page</Link>
         {/* We need a search bar here. */}
       </div>
       <div id="main">
         <Routes>
           <Route path="/" element={<HomePage />} />
           {/* <Route path="/" element={<AllMovies />} /> */}
-          <Route path="/" element={<AllMovies />}/>
+          <Route path="/AllMovies" element={<AllMovies />}/>
           {/* <Route path="/" element={<TopGenres />} />    <-- component doesn't exist yet */}
-          <Route path="/" element={<RatingRubric />} />
+          <Route path="/RatingRubric" element={<RatingRubric />} />
           {/* ↓ The following needs to appear to admins, and only admins ↓ */}
-          <Route path="/" element={<AllUsers />} />        
+          <Route path="/AllUsers" element={<AllUsers />} />        
           {/* ↓ The following need to alternate according to whether the user is 
           logged in. ↓ */}
-          <Route path="/" element={<LogIn />} />
-          <Route path="/" element={<Profile />} />
+          <Route path="/LogIn" element={<LogIn />} />
+          <Route path="/Profile" element={<Profile />} />
           {/* We need a search bar here. */}
         </Routes>
       </div>
