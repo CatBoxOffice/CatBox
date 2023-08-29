2b7d483389d4b952365d0911e0b88a5b61971e23
JamesGauvreau <james.longfrenchname@gmail.com>
2023-08-29 11:47:31 -0700

    ticket 49

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -1,7 +1,81 @@
+import React from "react";
+import { useState } from 'react';
+
 const AddReview = () => {
+  const [reviewData, setReviewData] = useState({
+    title: '',
+    content: '',
+    user: '',
+    // rating: '', <-- Not currently represented in the schema. 
+    movie: '',
+    Review_Tags: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setReviewData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('./server/api/addReview', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(reviewData),
+      });
+
+      if (response.ok) {
+        // Handle successful submission
+        console.log('Review added successfully');
+        setReviewData({
+          title: '',
+          description: '',
+          // rating: '', <-- Not currently represented in the schema. 
+          user: '',
+          movie: '',
+          content: '',
+        });
+
+        // Redirect to the original movie page after successful submission of a review for that movie.
+        movie.push('./movies/:movieId');
+      } else {
+        console.error('Failed to add review');
+      }
+    } catch (error) {
+      console.error('Error adding review', error);
+    }
+  };
+
   return (
-    <h1>Add Review</h1>
-  )
+    <div>
+      <h2>Add Review</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Movie:</label>
+        <input
+          type="text"
+          name="title"
+          value={reviewData.title}
+          onChange={handleChange}
+          required
+        />
+        <label>Review body:</label>
+        <input
+          type="text"
+          name="content"
+          value={reviewData.content}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Add Review</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddReview;
