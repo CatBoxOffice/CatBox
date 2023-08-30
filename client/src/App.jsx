2898adeb56826c29e96206a7a16230e33e6a59f9
JamesGauvreau <james.longfrenchname@gmail.com>
2023-08-30 12:39:06 -0700

    navbar & footer applied to all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 
 import Router from "./components/Router";
+import NavBar from "./components/bars/Navbar";
+import FooterBar from "./components/bars/FooterBar";
 
 const App = () => {
   return (
     <>
+    <NavBar />
       <h1>App</h1>
       <Router />
+      {/* <NavBar /> */}
+      <FooterBar />
     </>
   );
 };
 
 export default App;
+
