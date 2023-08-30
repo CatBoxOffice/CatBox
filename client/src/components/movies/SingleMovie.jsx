909b06bd05beabd70af82a4bf25617fe92b4a5d6
Loulian Liu <3l.liu444@gmail.com>
2023-08-30 10:59:34 -0400

    successfully pulled movie with params

--- client/src/components/movies/SingleMovie.jsx
+++ client/src/components/movies/SingleMovie.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const SingleMovie = () => {
+  const [movie, setMovie] = useState({});
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    const fetchMovie = async (id) => {
+      try {
+        const response = await fetch(`/api/movies/${id}`);
+        const result = await response.json();
+
+        setMovie(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchMovie(movieId);
+  }, []);
+
+  console.log(movie);
+
   return (
-    <h1>Single Movie</h1>
-  )
-}
+    <>
+      <section>
+        <img src={movie.poster} id="singleMoviePoster" />
+      </section>
+      <section></section>
+    </>
+  );
+};
 
 export default SingleMovie;
