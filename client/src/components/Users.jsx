13f6ec4077abfb733d82d2b0fbd280f868a63bcc
Loulian Liu <3l.liu444@gmail.com>
2023-08-30 21:37:20 -0400

    user page fleshed out

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Users = () => {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch(`/api/users`);
+      const result = await response.json();
+
+      result.sort((user1, user2) => {
+        if (user1.username < user2.username) {
+          return -1;
+        } else if (user1.username > user2.username) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
+      setUsers(result);
+    };
+
+    fetchUsers();
+  }, []);
+
+  const clickHandler = (id) => {
+    navigate(`/profile/${id}`);
+  };
+
   return (
-    <h1>Users</h1>
-  )
-}
+    <section id="userPage" className="flex">
+      <h1>Users</h1>
+      {users.map((user) => (
+        <section
+          key={user.id}
+          className="flex"
+          onClick={() => clickHandler(user.id)}
+        >
+          <section>
+            <img src={user.avatar} className="userAvatar" />
+          </section>
+          <section>
+            <h3>{user.username}</h3>
+            <p>Email: {user.email}</p>
+          </section>
+        </section>
+      ))}
+    </section>
+  );
+};
 
 export default Users;
