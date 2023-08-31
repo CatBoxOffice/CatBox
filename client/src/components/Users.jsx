494060eb6d69a50f3898343f5c6efc4abb835e70
Loulian Liu <3l.liu444@gmail.com>
2023-08-30 22:11:04 -0400

    successfully locked seeing all users behind admin priviledges

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -3,10 +3,32 @@ import { useNavigate } from "react-router-dom";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [isAdmin, setIsAdmin] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    if (localStorage.getItem(`token`)) {
+      const tokenArr = localStorage.getItem(`token`).split(`.`);
+      const { id } = JSON.parse(atob(tokenArr[1]));
+
+      checkIfAdmin(id);
+      fetchUsers();
+    }
+  }, []);
+
+  const checkIfAdmin = async (id) => {
+    try {
+      const response = await fetch(`api/users/${id}`);
+      const result = await response.json();
+
+      setIsAdmin(result.isAdmin);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchUsers = async () => {
+    try {
       const response = await fetch(`/api/users`);
       const result = await response.json();
 
@@ -21,10 +43,10 @@ const Users = () => {
       });
 
       setUsers(result);
-    };
-
-    fetchUsers();
-  }, []);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const clickHandler = (id) => {
     navigate(`/profile/${id}`);
@@ -33,21 +55,25 @@ const Users = () => {
   return (
     <section id="userPage" className="flex">
       <h1>Users</h1>
-      {users.map((user) => (
-        <section
-          key={user.id}
-          className="flex"
-          onClick={() => clickHandler(user.id)}
-        >
-          <section>
-            <img src={user.avatar} className="userAvatar" />
+      {isAdmin ? (
+        users.map((user) => (
+          <section
+            key={user.id}
+            className="flex"
+            onClick={() => clickHandler(user.id)}
+          >
+            <section>
+              <img src={user.avatar} className="userAvatar" />
+            </section>
+            <section>
+              <h3>{user.username}</h3>
+              <p>Email: {user.email}</p>
+            </section>
           </section>
-          <section>
-            <h3>{user.username}</h3>
-            <p>Email: {user.email}</p>
-          </section>
-        </section>
-      ))}
+        ))
+      ) : (
+        <p>You do not have Access</p>
+      )}
     </section>
   );
 };
