f31b1d5356a359a475aa262f22e1350c4cc8c25b
JamesGauvreau <james.longfrenchname@gmail.com>
2023-09-01 13:55:09 -0700

    all 'add review' buttons direct to the appropriate 'add reviews' page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -83,7 +83,7 @@ const Home = () => {
             <p>Rated {movie.rating}</p>
             <p>Studio: {movie.studio}</p>
             <p>Language: {movie.language}</p>
-            <button className="add-review-button" onClick={() => navigate(`/add-review/`)}>
+            <button className="add-review-button" onClick={() => navigate(`/add-review/${movie.id}`)}>
               ADD REVIEW
             </button>
           </div>
