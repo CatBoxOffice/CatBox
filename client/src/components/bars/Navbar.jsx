7c9d91d2f892badaa62b1617ab98dff25897389d
JamesGauvreau <james.longfrenchname@gmail.com>
2023-09-01 12:07:05 -0700

    nav bar edit

--- client/src/components/bars/Navbar.jsx
+++ client/src/components/bars/Navbar.jsx
@@ -1,31 +1,3 @@
-// import { Link } from "react-router-dom";
-// // import TopGenres from "./"; <-- component doesn't exist ye
-
-
-// function NavBar() {
-//   return (
-//     <div id="navbar">
-//       {/* ↓ We should add a logo here, if we're going to use one. ↓ */}
-//       <Link to="/">Home</Link>
-//       <Link to="/movies">All Movies</Link>
-//       {/* <Link to="/">TopGenres</Link>   <-- component doesn't exist yet*/}
-//       <Link to="/rubric">Rating Rubric</Link>
-//       {/* ↓ The following needs to appear to admins, and only admins ↓ */}
-//       <Link to="/users">All Users</Link>
-//       {/* ↓ The following need to alternate according to whether the user is 
-//         logged in. ↓ */}
-//       <Link to="/login">Log In</Link>
-//       <Link to="/profile">Profile Page</Link>
-//       {/* We need a search bar here. */}
-//     </div>
-//   );
-// }
-
-// export default NavBar;
-
-
-
-
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
@@ -45,7 +17,7 @@ function NavBar() {
   return (
     <div id="navbar">
       {/* Links to various pages */}
-      <Link to="/">Home</Link>
+      <Link to="/"><img src="https://github.com/CatBoxOffice/CatBox/blob/main/client/src/components/images/cinema_cat.png?raw=true" width="50"></img></Link>
       <Link to="/movies">All Movies</Link>
       <Link to="/rubric">Rating Rubric</Link>
       {/* Links visible to admins only */}
@@ -53,7 +25,7 @@ function NavBar() {
       {/* Conditional links based on user login status */}
       <Link to="/login">Log In</Link>
       <Link to="/profile">Profile Page</Link>
-
+<br></br><br></br>
       {/* Search bar */}
       <form onSubmit={handleSearchSubmit}>
         <input
