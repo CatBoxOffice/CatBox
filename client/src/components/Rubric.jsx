c8577d4bc81def86d344c4d2095bfc56569b7c63
Kevin Anderson <ksacamera@gmail.com>
2023-09-01 11:13:52 -0700

    fixed wording of Rubric and added 3 IMGs

--- client/src/components/Rubric.jsx
+++ client/src/components/Rubric.jsx
@@ -1,19 +1,27 @@
+import cheeseburger from './images/Cheeseburger3.png';
+import kittylitter from './images/dirty_litter_box.png';
+import catmeme from './images/meme2.png';
+
 const Rubric = () => {
   return (
     <>
       <h1>Our Stanley Rubric</h1>
       <p>
-        Here at CatBoxOffice, good a film has cheezburger and a bad film gets
-        scattered with the scat in catbox. Keep this in mind when you’re
+        Here at CatBoxOffice, a good film is rewarded with a cheezburger and a bad film gets
+        scattered within the scat of the catbox. Keep this in mind when you’re
         reviewing a film. Do you reward it with a juicy Cheezburger? Or do you
         banish it into the lost land of litter to wallow for an eternity?
       </p>
       <p>How do
-        we know if a film has Cheezburger you ask? If a film receives a 75% or
-        higher rating (scale of 1-100) it is rewarded with a big ol’ 80/20
-        burgski. Anything else plummets to the catbox stinkfest where it
+        we know if a film has Cheezburger you ask? If a film receives a 75 or
+        higher grade (scale of 1-100) it is rewarded with a big ol’ 80/20
+        juicy Burger. Anything else plummets to the catbox stinkfest where it
         belongs. It is up to you to choose it’s destiny!</p>
+      <img src={cheeseburger} alt="Has Cheezburger" style={{ width: '350px', height: 'auto' }} />
+      <img src={catmeme} alt="Cat Meme" style={{ width: '350px', height: 'auto' }} />
+      <img src={kittylitter} alt="Cat Scat" style={{ width: '350px', height: 'auto' }} />
     </>
+
   );
 };
 
