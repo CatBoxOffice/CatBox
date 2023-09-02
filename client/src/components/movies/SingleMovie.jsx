bf3e3f734607d620a1c83e7e717666dd8c3822c3
Kevin Anderson <ksacamera@gmail.com>
2023-09-01 23:00:58 -0700

    singlemovie page now has avatars, usernames, and grades/IMGs

--- client/src/components/movies/SingleMovie.jsx
+++ client/src/components/movies/SingleMovie.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import cheeseburger from "../images/Cheeseburger3.png";
+import kittylitter from "../images/dirty_litter_box.png";
 
 const SingleMovie = () => {
   const [movie, setMovie] = useState({});
@@ -11,8 +13,16 @@ const SingleMovie = () => {
       try {
         const response = await fetch(`/api/movies/${id}`);
         const movie = await response.json();
+        
+        const reviewsWithUsername = await Promise.all(
+          movie.reviews.map(async (review) => {
+            const userResponse = await fetch(`/api/users/${review.userId}`);
+            const userData = await userResponse.json();
+            return { ...review, username: userData.username, avatar: userData.avatar };
+          })
+        );
 
-        setMovie(movie);
+        setMovie({ ...movie, reviews: reviewsWithUsername });
       } catch (error) {
         console.log(error);
       }
@@ -41,25 +51,43 @@ const SingleMovie = () => {
               ))
             : "N/A"}
         </p>
-
         <p>Rated {movie.rating}</p>
+        <p>Director: {movie.director}</p>
+        <p>Studio: {movie.studio}</p>
+        <p>Language: {movie.language}</p>
         <button onClick={clickHandler}>Add Review</button>
       </section>
 
       <section>
-        <h1>{movie.title}</h1>
-        <section id="movieInfo">
-          <p>Director: {movie.director}</p>
-          <p>Studio: {movie.studio}</p>
-          <p>Language: {movie.language}</p>
-        </section>
-
         <section id="reviewsSection">
           {movie.reviews ? (
             <div>
               {movie.reviews.map((review) => (
                 <section key={review.id}>
+                  <div style={{ display: "flex", alignItems: "center" }}>
+          <img
+            src={review.avatar} // Use the avatar from userData
+            id="navbarAvatar"
+            alt="User Avatar"
+            style={{ width: "40px", height: "40px", marginRight: "10px" }}
+          />
+          <h2>{review.username}</h2>
+        </div>
                   <h3>{review.title}</h3>
+                  <h3>Grade: {review.grade}</h3>
+                  {review.grade >= 75 ? (
+                    <img
+                      src={cheeseburger}
+                      alt="Has Cheezburger"
+                      style={{ width: "120px", height: "100px" }}
+                    />
+                  ) : (
+                    <img
+                      src={kittylitter}
+                      alt="Cat Scat"
+                      style={{ width: "120px", height: "100px" }}
+                    />
+                  )}
                   <p>{review.content}</p>
                 </section>
               ))}
---
+++ prisma/migrations/20230902050810_include_username_reviews/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Review" ADD COLUMN     "username" VARCHAR(255) NOT NULL DEFAULT '';
