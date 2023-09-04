1fa58f67ff89ddc9784261f5fb1e1a344ba3f93d
Lisa Wu Zheng <lisawuzheng@lisas-imac.mynetworksettings.com>
2023-09-04 01:15:53 -0400

    sticky nav and footer bars

--- client/src/components/bars/Navbar.jsx
+++ client/src/components/bars/Navbar.jsx
@@ -45,49 +45,53 @@ function NavBar({ loggedIn, setLoggedIn }) {
     if (searchQuery.trim() !== "") {
       // Navigate to search results page with the search query as a parameter
       navigate(`/search?q=${encodeURIComponent(searchQuery)}`);
+      setSearchQuery("");
     }
   };
 
   return (
-    <div id="navbar">
-      {/* Links to various pages */}
-      <Link to={"/"}>
-        <img
-          src="https://github.com/CatBoxOffice/CatBox/blob/main/client/src/components/images/cinema_cat.png?raw=true"
-          width="50"
-        ></img>
-      </Link>
-      <Link to={"/movies"}>All Movies</Link>
-      <Link to={"/rubric"}>Grading Rubric</Link>
-      {/* Links visible to admins only */}
-      {isAdmin ? <Link to={"/users"}>All Users</Link> : null}
-
-      <br></br>
-      <br></br>
-      <form onSubmit={handleSearchSubmit}>
-        <input
-          type="text"
-          placeholder="Search movies..."
-          value={searchQuery}
-          onChange={(event) => setSearchQuery(event.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
-      {userId ? (
-        <section className="flex" id="navbarProfile">
-          <button onClick={signOutHandler} style={{ width: 50, fontSize: 8 }}>
-            Sign Out
-          </button>
+    <>
+      <div id="navbar">
+        {/* Links to various pages */}
+        <Link to={"/"}>
           <img
-            src={userData.avatar}
-            id="navbarAvatar"
-            onClick={() => navigate(`/profile/${userId}`)}
+            src="https://github.com/CatBoxOffice/CatBox/blob/main/client/src/components/images/cinema_cat.png?raw=true"
+            width="50"
+          ></img>
+        </Link>
+        <Link to={"/movies"}>All Movies</Link>
+        <Link to={"/rubric"}>Grading Rubric</Link>
+        {/* Links visible to admins only */}
+        {isAdmin ? <Link to={"/users"}>All Users</Link> : null}
+
+        <br></br>
+        <br></br>
+        <form onSubmit={handleSearchSubmit}>
+          <input
+            type="text"
+            placeholder="Search movies..."
+            value={searchQuery}
+            onChange={(event) => setSearchQuery(event.target.value)}
           />
-        </section>
-      ) : (
-        <Link to={"/login"}>Log In</Link>
-      )}
-    </div>
+          <button type="submit">Search</button>
+        </form>
+        {userId ? (
+          <section className="flex" id="navbarProfile">
+            <button onClick={signOutHandler} style={{ width: 50, fontSize: 8 }}>
+              Sign Out
+            </button>
+            <img
+              src={userData.avatar}
+              id="navbarAvatar"
+              onClick={() => navigate(`/profile/${userId}`)}
+            />
+          </section>
+        ) : (
+          <Link to={"/login"}>Log In</Link>
+        )}
+      </div>
+      <div className="spacer"></div>
+    </>
   );
 }
 
